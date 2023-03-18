d41c9dc51c989ed301ecbbadef3ff301da15a40e
Egor Kovalchuk <kovalchuk.egor1357@gmail.com>
2023-03-18 13:29:21 +0200

    day2

--- Arkanoid/test.cpp
+++ Arkanoid/test.cpp
@@ -1,23 +1,69 @@
 #include <iostream>
 #include <SDL3/SDL.h>
 
+#define SCREEN_WIDTH 1280
+#define SCREEN_HEIGHT 720
+
+typedef struct {
+	SDL_Renderer  *renderer;
+	SDL_Window *window;
+} App;
+
+App app;
+
+void initSDL(void)
+{
+	uint32_t rendererFlags, windowFlags;
+	rendererFlags = SDL_RENDERER_ACCELERATED;
+	windowFlags = NULL;
+
+	if (SDL_Init(SDL_INIT_VIDEO) < 0)
+	{
+		printf("Couldn't initialize SDL: %s\n", SDL_GetError());
+		exit(1);
+	}
+
+	app.window = SDL_CreateWindow("Arkanoid", SCREEN_WIDTH, SCREEN_HEIGHT, windowFlags);
+
+	if (!app.window)
+	{
+		printf("Failed to open %d x %d window: %s\n", SCREEN_WIDTH, SCREEN_HEIGHT, SDL_GetError());
+		exit(1);
+	}
+
+	SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, "linear");
+
+	app.renderer = SDL_CreateRenderer(app.window, NULL, rendererFlags);
+
+	if (!app.renderer)
+	{
+		printf("Failed to create renderer: %s\n", SDL_GetError());
+		exit(1);
+	}
+}
+void doInput(void)
+{
+	SDL_Event event;
+
+	while (SDL_PollEvent(&event))
+	{
+		switch (event.type)
+		{
+			case SDL_EVENT_QUIT:
+				exit(0);
+				break;
+			default:
+				break;
+		}
+	}
+}
+
 int main()
 {
-	if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
-		std::cout << "SDL_Init Error: " << SDL_GetError() << std::endl;
-		return 1;
+	initSDL();
+	while (1)
+	{
+		doInput();
 	}
-	SDL_Window* win = SDL_CreateWindow("Hello World!", 800, 600, NULL);
-	if (win == nullptr) {
-		std::cout << "SDL_CreateWindow Error: " << SDL_GetError() << std::endl;
-		return 1;
-	}
-
-	SDL_Renderer* ren = SDL_CreateRenderer(win, "Renderer", NULL);
-	if (ren == nullptr) {
-		std::cout << "SDL_CreateRenderer Error: " << SDL_GetError() << std::endl;
-		return 1;
-	}
-
 	return 0;
 }
